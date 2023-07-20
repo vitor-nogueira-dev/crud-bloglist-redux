76391f6d943347c312bd49036e6356aada6a4195
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-20 16:09:52 -0300

    feat: creating action thunk get list

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,4 +1,8 @@
+import { Dispatch } from "react";
+import { ThunkAction } from "redux-thunk";
+import { initialState } from "../redux/listReducer";
 import { SET_LIST, SET_NAME, SET_PAGINATION } from "./constants";
+import { IActionReducer } from "@/interfaces/IActionReducer";
 
 export const ACTION_SET_NAME = (name: string) => ({
     type: SET_NAME,
@@ -15,3 +19,21 @@ export const ACTION_SET_PAGINATION = (pagination: any) => ({
     payload: pagination,
   });
 
+export const ACTION_GET_LIST = (
+    limit: number,
+    offset: number
+): ThunkAction<void, typeof initialState, null, IActionReducer> => {
+    return async (dispatch: Dispatch<IActionReducer>) => {
+        const response = await fetch(
+            `https://dev.codeleap.co.uk/careers/?limit=${limit}&offset=${offset}`
+        );
+        const data = await response.json();
+        dispatch(ACTION_SET_LIST(data.results));
+        dispatch(ACTION_SET_PAGINATION({
+            results: data.results,
+            count: data.count,
+            next: data.next,
+            previous: data.previous,
+        }));
+    };
+};
