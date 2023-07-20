99bda66c33f6d840985e2e66c853cca5208b17c2
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-20 12:05:31 -0300

    feat: implementing case set name in list reducer

--- src/redux/listReducer.ts
+++ src/redux/listReducer.ts
@@ -1,13 +1,19 @@
+import { SET_NAME } from "@/actions/constants";
 import { IActionReducer } from "@/interfaces/IActionReducer";
 import { IListState } from "@/interfaces/IListState";
 
 const initialState: IListState = {
     items: [],
+    name: '',
 };
 
 const listReducer = (state = initialState, action: IActionReducer) => {
     switch (action.type) {
-        case '':
+        case SET_NAME:
+            return {
+                ...state,
+                name: action.payload,
+            };
         case '':
         default:
             return state;
