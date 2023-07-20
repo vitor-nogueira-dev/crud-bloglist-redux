582e7f69260b82220bba7de1b6406d7ab6d8fe4e
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-20 16:09:26 -0300

    feat: creating action set list and set pagination

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,5 +1,17 @@
+import { SET_LIST, SET_NAME, SET_PAGINATION } from "./constants";
 
-export const SET_NAME = (name: string) => ({
-    type: 'SET_NAME',
+export const ACTION_SET_NAME = (name: string) => ({
+    type: SET_NAME,
     payload: name,
 });
+
+export const ACTION_SET_LIST = (list: any) => ({
+    type: SET_LIST,
+    payload: list,
+})
+
+export const ACTION_SET_PAGINATION = (pagination: any) => ({
+    type: SET_PAGINATION,
+    payload: pagination,
+  });
+
