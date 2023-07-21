5517d71c812dc0e11e110030487c95140df866b5
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-21 18:58:55 -0300

    style: adding signup responsiveness

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -19,9 +19,9 @@ export default function Signup({ }: Props) {
     }
     return (
         <section className='bg-[#DDDDDD] h-screen m-auto border flex items-center justify-center'>
-            <form className='bg-[#ffffff] lg:w-[500px] h-[205px] rounded-[16px] flex flex-col justify-evenly border border-[#CCCCCC]
+            <form className='bg-[#ffffff] w-[320px] lg:w-[500px] md:w-[500px] h-[230px] rounded-[16px] flex flex-col justify-evenly border border-[#CCCCCC]
              p-6 py-10 shadow-md'>
-                <h1 className='text-2xl font-bold leading-none text-[#000000] md:text-2xl lg:text-2xl mb-4'>Welcome to CodeLeap network!</h1>
+                <h1 className='text-xl font-bold leading-none text-[#000000] md:text-2xl lg:text-2xl mb-4'>Welcome to CodeLeap network!</h1>
                 <div className="mb-6">
                     <label htmlFor="name" className="block mb-2 text-[16px] font-normal text-[#000000] leading-4">Please enter your username</label>
                     <input
