ece00cefa200ef157a51f4bcc33c2a7e6ce9b68e
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-21 10:42:25 -0300

    chore: installing bootstrap lib and date-fns

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import store from '@/redux/store'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
