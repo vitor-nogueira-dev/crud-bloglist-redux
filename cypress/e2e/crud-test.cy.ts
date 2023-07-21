564c5408fcfe3ef4252a3888929a2deb88b380cb
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-21 16:00:41 -0300

    test: should enable button when name is entered and clicked

--- cypress/e2e/crud-test.cy.ts
+++ cypress/e2e/crud-test.cy.ts
@@ -7,4 +7,11 @@ describe('CRUD Test', () => {
   it('should disable button when name is not entered', () => {
     cy.get('button[type="button"]').should('be.disabled');
   });
+
+  it('should enable button when name is entered and clicked', () => {
+    cy.get('#name').type('User Test');
+    cy.get('button[type="button"]').should('be.enabled').click();
+    cy.url().should('include', '/feed');
+    cy.contains('Welcome back, User Test :)');
+  });
 });
