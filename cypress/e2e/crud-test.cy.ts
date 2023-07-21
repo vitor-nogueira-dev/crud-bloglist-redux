269601cd7c107aafb69759dce5d9ee7cee16b6e3
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-21 16:00:19 -0300

    test: should disable button when name is not entered

--- cypress/e2e/crud-test.cy.ts
+++ cypress/e2e/crud-test.cy.ts
@@ -1,6 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    // lcoalhost 3000
-    cy.visit('http://localhost:3000')
-  })
-})
+
+describe('CRUD Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000').wait(1000);
+  });
+
+  it('should disable button when name is not entered', () => {
+    cy.get('button[type="button"]').should('be.disabled');
+  });
+});
