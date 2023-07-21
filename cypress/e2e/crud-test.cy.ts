ac5ec0729e8cb83aec2241e34aa75bb8b2db3d5d
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-21 16:01:12 -0300

    test: should create a post

--- cypress/e2e/crud-test.cy.ts
+++ cypress/e2e/crud-test.cy.ts
@@ -14,4 +14,16 @@ describe('CRUD Test', () => {
     cy.url().should('include', '/feed');
     cy.contains('Welcome back, User Test :)');
   });
+
+  it('should create a post', () => {
+    cy.get('#name').type('User Test');
+    cy.get('button[type="button"]').should('be.enabled').click();
+    cy.url().should('include', '/feed');
+    cy.contains('Welcome back, User Test :)');
+    cy.get('#title').type('Post Test');
+    cy.get('#content').type('Content Test');
+    cy.contains('create').click().wait(2000);
+    cy.contains('Post Test');
+    cy.contains('Content Test');
+  });
 });
