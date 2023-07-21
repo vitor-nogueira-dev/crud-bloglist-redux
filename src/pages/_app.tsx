5a881f69fc02b00030bd953005f715f82a450776
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-21 16:26:30 -0300

    style: creating logout style button and importing it in the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
+import '@/styles/logout.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
