b2a5fa5a4412d6f577cc4efe3545eecb70a22f8b
Vitor Nogueira <vslnogueira96@gmail.com>
2023-07-22 12:56:24 -0300

    feat: changing title application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import store from '@/redux/store'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import { Provider } from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,9 +8,16 @@ import '@/styles/globals.css'
 import '@/styles/logout.css'
 import 'aos/dist/aos.css';
 
+const metadata = {
+  title: 'Blog List',
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head >
+        <title>{metadata.title}</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   )
